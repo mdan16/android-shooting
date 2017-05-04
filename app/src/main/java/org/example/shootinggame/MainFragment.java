3b30d257121b982f4fd215091051f7818d18e9ff
mdan16 <t.t.mina16@gmail.com>
2017-05-04 18:31:26 +0900

    first commit

---
+++ app/src/main/java/org/example/shootinggame/MainFragment.java
@@ -0,0 +1,30 @@
+package org.example.shootinggame;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+    private AlertDialog mDialog;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        View aboutButton = rootView.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(R.string.about_title);
+                builder.setMessage(R.string.about_text);
+                builder.setCancelable(false);
+                builder.setPositiveButton(R.string.ok_label, null);
+                mDialog = builder.show();
+            }
+        });
+        return rootView;
+    }
+}
